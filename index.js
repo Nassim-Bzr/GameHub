0dd769ed5d4fac2e7523a10b8ecfa4148991175e
Nassimou78 <Nassimbzr78@gmail.com>
2022-06-08 11:04:09 +0200

    Etape 3 terminer

--- index.js
+++ index.js
@@ -18,6 +18,18 @@ app.get("/", (req, res) => {
     res.locals.pageTitle = "Accueil"
     res.render("index");
   });
+  app.get("/game/diceRoller", (req, res) => {
+    // on demande à express de générer le html à renvoyer au client sur
+    // a partir d'un template donné
+    res.locals.pageTitle = "DiceRoller"
+    res.render("diceRoller");
+  });
+  app.get("/game/fourchette", (req, res) => {
+    // on demande à express de générer le html à renvoyer au client sur
+    // a partir d'un template donné
+    res.locals.pageTitle = "Fourchette"
+    res.render("fourchette");
+  });
 
   app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`);
